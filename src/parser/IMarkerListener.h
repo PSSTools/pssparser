a75c60f053b13ef8f8317297bc2898dc5ecefad4
Matthew Ballance <matt.ballance@gmail.com>
2020-09-13 20:57:07 -0400

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

---
+++ src/parser/IMarkerListener.h
@@ -0,0 +1,15 @@
+/*
+ * IMarkerListener.h
+ *
+ *  Created on: Sep 13, 2020
+ *      Author: ballance
+ */
+
+#pragma once
+
+class IMarkerListener {
+public:
+	virtual ~IMarkerListener() { }
+
+
+};
